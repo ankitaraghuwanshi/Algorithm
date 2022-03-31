989c62ba55b15c5fa80022a1a4737223f7062a6a
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-04-01 00:08:26 +0530

    [Ankita] Add: Program for permutation of string

---
+++ AlgorithmProblem/Permutation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    internal class StringPermutation
+    {
+        public void Input()
+        {
+            Console.Write("Enter the word for permutation: ");
+            string Word = Console.ReadLine();
+            Recursion(Word, 0);
+        }
+
+        public static void Recursion(string word, int start)
+        {
+            int Length = word.Length;
+
+            for (int i = start; i < Length; i++)
+            {
+                word= (Swap(word, start, i));
+                Console.WriteLine(word);
+                Recursion(word, start + 1);
+                word = Swap(word, start, i);
+            }
+        }
+        public static string Swap(string word, int start, int end)
+        {
+            char store;
+            char[] charArray = word.ToCharArray();
+            store = charArray[start];
+            charArray[start] = charArray[end];
+            charArray[end] = store;
+            string newword = new string(charArray);
+            return newword;
+        }
+    }
+}
---
+++ AlgorithmProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿using AlgorithmPrograms;
+using System;
+
+namespace AlgorithmProblem
+{
+    public class program
+    {
+        public static void Main(String[] args)
+        {
+            StringPermutation stringPermutation = new StringPermutation();
+            stringPermutation.Input();
+        }
+    }
+}
