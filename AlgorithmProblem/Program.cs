e541c5097cdd4339687727140dda674bdfa39132
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-04-03 00:09:01 +0530

    [Ankita] Add: Program for bubbleSort

--- AlgorithmProblem/Program.cs
+++ AlgorithmProblem/Program.cs
@@ -1,4 +1,4 @@
-﻿using AlgorithmPrograms;
+﻿using AlgorithmProblem;
 using System;
 
 namespace AlgorithmProblem
@@ -7,8 +7,30 @@ namespace AlgorithmProblem
     {
         public static void Main(String[] args)
         {
-            StringPermutation stringPermutation = new StringPermutation();
-            stringPermutation.Input();
+           // StringPermutation stringPermutation = new StringPermutation();
+            //stringPermutation.Input();
+            Console.WriteLine("1: For String Permutation");
+            Console.WriteLine("2: For Bubble Sort");
+            int option = int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    StringPermutation stringPermutation = new StringPermutation();
+                    stringPermutation.Input();
+                    break;
+                case 2:
+                    BubbleSort bubbleSort = new BubbleSort();
+                    bubbleSort.BubbleSorting();
+                    Console.WriteLine();
+                    break;
+                default:
+                    Console.WriteLine("press a valid number");
+                    break;
+            }
         }
+
     }
 }
+        
+        
+    
